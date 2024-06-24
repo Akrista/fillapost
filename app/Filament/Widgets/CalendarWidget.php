d567fac9592b3e464edfdac200454e9de866185a
Jorge Thomas - Akrista <23145794+Akrista@users.noreply.github.com>
2024-06-24 07:50:17 -0400

    Feat: Added fullcalendar, WIP create a section with a calendar to manage post scheduling and the sorts

---
+++ app/Filament/Widgets/CalendarWidget.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
+
+class CalendarWidget extends FullCalendarWidget
+{
+    /**
+     * FullCalendar will call this function whenever it needs new event data.
+     * This is triggered when the user clicks prev/next or switches views on the calendar.
+     */
+    public function fetchEvents(array $fetchInfo): array
+    {
+        // You can use $fetchInfo to filter events by date.
+        // This method should return an array of event-like objects. See: https://github.com/saade/filament-fullcalendar/blob/3.x/#returning-events
+        // You can also return an array of EventData objects. See: https://github.com/saade/filament-fullcalendar/blob/3.x/#the-eventdata-class
+        return [];
+    }
+}
